5b83239d73bde67f5503b785633290e4d88e54db
Adam Ezzeldin <adamezzeldin2016@gmail.com>
2024-07-04 12:59:56 +0100

    Adds a single button and mapping works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import testImage from './images/Best_JJK_Character.webp'
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  const buttons = [
+    {imageSrc: testImage, buttonText: 'nanami number 1'},
+  ]
+
+  const [selectedProblem, setSelectedProblem] = useState(null)
+
+  const buttonList = [
+    buttons.map(({imageSrc, buttonText}, index) => 
+      <button key={index}>
+        <img src={imageSrc} alt={buttonText} style={{ width: '50px', height: '50px', marginRight: '8px' }}
+        />
+        {buttonText}
+        </button>
+    )
+  ]
+
+  useEffect(() => {
+
+  });
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div classname='home_page'>
+      <header classname='home_heading'>
+        <h1>What is the Problem?</h1>
       </header>
+      <div classname='button-list'>
+        {buttonList}
+      </div>
     </div>
   );
 }
